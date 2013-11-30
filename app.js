25207ded99cfe2bba3f52700589eca4fe52c8fa4
Kana Natsuno <dev@whileimautomaton.net>
2013-12-01 03:09:25 +0900

    Implement makeInitalWorld

--- app.js
+++ app.js
@@ -1,6 +1,105 @@
+// Design:
+//
+// * There are several regions in the game;
+//   Sheep Stock,
+//   Field,
+//   Hand,
+//   Deck,
+//   Discard pile,
+//   Exile.
+// * A region contains a pile of cards.
+// * A pile of cards is expressed as an array.
+// * The first card in an array is corresponding to
+//   the bottom card in a pile or the first card put into the pile.
+// * The last card in an array is corresponding to
+//   the top card in a pile or the last card put into the pile.
+
 var shephy = {};
 
 (function (S, $) {
+  S.CARD_TYPE_SHEEP = 'Sheep';
+  S.CARD_TYPE_EVENT = 'Event';
+
+  function random(n) {
+    return Math.floor(Math.random() * n);
+  }
+
+  function shuffle(xs) {
+    for (var i = 0; i < xs.length; i++) {
+      var j = random(xs.length - i);
+      var tmp = xs[i];
+      xs[i] = xs[j];
+      xs[j] = tmp;
+    }
+  }
+
+  function makeSheepStockPile(n) {
+    var cards = [];
+    for (var i = 0; i < 7; i++) {
+      cards.push({
+        name: n + '',
+        type: S.CARD_TYPE_SHEEP,
+        count: n
+      });
+    }
+    return cards;
+  }
+
+  function makeEventCard(name) {
+    // TODO: Implement effects when a card is played.
+    return {
+      name: name,
+      type: S.CARD_TYPE_EVENT
+    };
+  }
+
+  function makeInitalDeck(n) {
+    var names = [
+      'All-purpose Sheep',
+      'Be Fruitful',
+      'Be Fruitful',
+      'Be Fruitful',
+      'Crowding',
+      'Dominion',
+      'Dominion',
+      'Falling Rock',
+      'Fill the Earth',
+      'Flourish',
+      'Golden Hooves',
+      'Inspirationi',
+      'Lightning',
+      'Meteor',
+      'Multiply',
+      'Plague',
+      'Planning Sheep',
+      'Sheep Dog',
+      'Shephion',
+      'Slump',
+      'Storm',
+      'Wolves'
+    ];
+    var cards = names.map(makeEventCard);
+    shuffle(cards);
+    return cards;
+  }
+
+  S.makeInitalWorld = function () {
+    var sheepStock = {};
+    [1, 3, 10, 30, 100, 300, 1000].forEach(function (n) {
+      sheepStock[n] = makeSheepStockPile(n);
+    });
+
+    var initialSheepCard = sheepStock[1].pop();
+
+    return {
+      sheepStock: sheepStock,
+      field: [initialSheepCard],
+      enemySheepCount: 1,
+      deck: makeInitalDeck(),
+      discardPile: [],
+      exile: []
+    };
+  };
 })(shephy, jQuery);
 
 // vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
--- spec.js
+++ spec.js
@@ -1,5 +1,33 @@
 describe('shephy', function () {
   var S = shephy;
+
+  describe('makeInitalWorld', function () {
+    it('makes a new world', function () {
+      var w = S.makeInitalWorld();
+
+      [1, 3, 10, 30, 100, 300, 1000].forEach(function (n) {
+        expect(w.sheepStock[n].length).toEqual(n == 1 ? 6 : 7);
+        w.sheepStock[n].forEach(function (c) {
+          expect(c.type).toEqual(S.CARD_TYPE_SHEEP);
+          expect(c.count).toEqual(n);
+        });
+      });
+
+      expect(w.field.length).toEqual(1);
+      expect(w.field[0].type).toEqual(S.CARD_TYPE_SHEEP);
+      expect(w.field[0].count).toEqual(1);
+
+      expect(w.enemySheepCount).toEqual(1);
+
+      expect(w.deck.length).toEqual(22);
+      w.deck.forEach(function (c) {
+        expect(c.type).toEqual(S.CARD_TYPE_EVENT);
+      });
+
+      expect(w.discardPile.length).toEqual(0);
+      expect(w.exile.length).toEqual(0);
+    });
+  });
 });
 
 // vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
